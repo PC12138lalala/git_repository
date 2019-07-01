07845b3873634c67d3c6e4c9ef899a953c0c28c9
PC12138lalala <q1053893799@126.com>
2019-07-01 10:13:59 +0800

    add Spring Cloud

---
+++ hello-spring-cloud/hello-spring-cloud-web-admin-feign/src/main/java/com/funtl/hello/spring/cloud/web/admin/feign/Hystrix/AdminServiceHystrix.java
@@ -0,0 +1,12 @@
+package com.funtl.hello.spring.cloud.web.admin.feign.Hystrix;
+
+import com.funtl.hello.spring.cloud.web.admin.feign.service.AdminService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdminServiceHystrix implements AdminService {
+    @Override
+    public String sayHi(String message) {
+        return String.format("hi ur message is: %s ,but request is bad", message);
+    }
+}
