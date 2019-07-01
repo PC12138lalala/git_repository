cd35a7df4625c5849d86242d5d7150efab242bb2
PC12138lalala <q1053893799@126.com>
2019-07-01 10:00:41 +0800

    delete all

--- RockstarManager Maven Webapp/src/main/java/com/service/ForumService.java
+++
@@ -1,9 +0,0 @@
-package com.service;
-
-import com.util.Pageing;
-
-public interface ForumService {
-
-	public void pageingBasic(Pageing p);
-	public void updateStatus(String id,String type,String status);
-}
--- a.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-a=`echo "obase=2;$1"|bc`
-b=$(($a%10))
-echo "$b"
-
-echo "exit,sb!"
