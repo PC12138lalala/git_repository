a9712684e4c4dbbc4e01c8f92ca3d119fbb7c322
PC12138lalala <q1053893799@126.com>
2019-07-03 18:11:00 +0800

    add protal

---
+++ corporate_portal/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <!--<img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <HeaderVue/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import HeaderVue from "./components/HeaderNav.vue";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld,
+    HeaderVue
+  }
+};
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+body{
+  position: relative;
+}
+</style>
