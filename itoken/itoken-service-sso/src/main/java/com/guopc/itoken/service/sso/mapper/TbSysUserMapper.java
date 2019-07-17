d32865c98e41f0e9d605aeb0ac4845fc19fcf254
PC12138lalala <q1053893799@126.com>
2019-07-17 10:17:46 +0800

    添加修改单点登陆相关配置

---
+++ itoken/itoken-service-sso/src/main/java/com/guopc/itoken/service/sso/mapper/TbSysUserMapper.java
@@ -0,0 +1,7 @@
+package com.guopc.itoken.service.sso.mapper;
+
+import com.guopc.itoken.common.domain.domain.TbSysUser;
+import tk.mybatis.mapper.MyMapper;
+
+public interface TbSysUserMapper extends MyMapper<TbSysUser> {
+}
